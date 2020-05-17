e6658de719bcfbf93f82a1dc94f0c14e25b8bff8
ammartin8 <ammartin8249@gmail.com>
2020-05-17 19:19:36 -0400

    Add new image to footer and worked on footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import dinnerPlate from "../images/dinnerPlate.svg";
 // Importing React Bootstrap Elements
 import { Container, Col, Row } from "react-bootstrap";
 
@@ -24,7 +24,7 @@ const Footer = (props) => {
         <Col
           className="top-cities d-flex"
           style={{ flexDirection: "column" }}
-          xs={5}
+          xs={6} sm={3}
         >
           <ul className="p-0 footer-city-list">
             <strong>{props.cityTitle}</strong>
@@ -103,22 +103,93 @@ const Footer = (props) => {
         <Col
           className="cuisine-container d-flex"
           style={{ flexDirection: "column" }}
-          xs={5}
+          xs={6} sm={3}
         >
           <ul className="p-0 footer-city-list">
             <strong>{props.cuisineTitle}</strong>
-            <li><a href="#main-header" onClick={()=> {props.handleCuisineIdUpdate(152)}}>African</a></li>
-            <li><a href="#main-header" onClick={()=> {props.handleCuisineIdUpdate(3)}}>Asian</a></li>
-            <li><a href="#main-header" onClick={()=> {props.handleCuisineIdUpdate(1)}}>American</a></li>
-            <li><a href="#main-header" onClick={()=> {props.handleCuisineIdUpdate(73)}}>Mexican</a></li>
-            <li><a href="#main-header" onClick={()=> {props.handleCuisineIdUpdate(55)}}>Italian</a></li>
-            <li><a href="#main-header" onClick={()=> {props.handleCuisineIdUpdate(70)}}>Mediterranean</a></li>
-            <li><a href="#main-header" onClick={()=> {props.handleCuisineIdUpdate(308)}}>Vegetarian</a></li>
+            <li>
+              <a
+                href="#main-header"
+                onClick={() => {
+                  props.handleCuisineIdUpdate(152);
+                }}
+              >
+                African
+              </a>
+            </li>
+            <li>
+              <a
+                href="#main-header"
+                onClick={() => {
+                  props.handleCuisineIdUpdate(3);
+                }}
+              >
+                Asian
+              </a>
+            </li>
+            <li>
+              <a
+                href="#main-header"
+                onClick={() => {
+                  props.handleCuisineIdUpdate(1);
+                }}
+              >
+                American
+              </a>
+            </li>
+            <li>
+              <a
+                href="#main-header"
+                onClick={() => {
+                  props.handleCuisineIdUpdate(73);
+                }}
+              >
+                Mexican
+              </a>
+            </li>
+            <li>
+              <a
+                href="#main-header"
+                onClick={() => {
+                  props.handleCuisineIdUpdate(55);
+                }}
+              >
+                Italian
+              </a>
+            </li>
+            <li>
+              <a
+                href="#main-header"
+                onClick={() => {
+                  props.handleCuisineIdUpdate(70);
+                }}
+              >
+                Mediterranean
+              </a>
+            </li>
+            <li>
+              <a
+                href="#main-header"
+                onClick={() => {
+                  props.handleCuisineIdUpdate(308);
+                }}
+              >
+                Vegetarian
+              </a>
+            </li>
           </ul>
         </Col>
 
-        <Col xs={2} className="d-flex justify-content-end">
-          <div className="pr-3">
+        <Col
+          className="illustrate-container d-flex justify-content-center"
+          style={{ flexDirection: "column", minWidth: "5em"}}
+          xs={12} sm={3}
+        >
+          <img src={dinnerPlate} alt="logo" style={{width: "15rem", minWidth: "15rem", alignSelf: "center"}}></img>
+        </Col>
+
+        <Col xs={12} sm={2} className="d-flex justify-content-end">
+          <div>
             <a href="#main-header">
               <FaChevronUp className="chevron-top-page" />
             </a>
@@ -150,7 +221,7 @@ const Footer = (props) => {
           </IconContext.Provider>
         </Row>
         <Row sm={12}>
-          <span className="mx-auto py-3">
+          <span className="mx-auto py-3 text-center">
             &copy; Copyright 2020 | All Rights Reserved
           </span>
         </Row>
